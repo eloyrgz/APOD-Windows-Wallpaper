d05c47fe858c725538ce55e970b3feb9db6945de
Eloy R Romero <eloyrgz@users.noreply.github.com>
2020-08-04 12:40:21 +0200

    Update apod_wallpaper.py

--- apod_wallpaper.py
+++ apod_wallpaper.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python
 # -*-coding:utf-8-*-
 import sys
+import warnings
 import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import logging
 import os
 from PIL import Image
 import io
 import configparser
-import win32api, win32con, win32gui
-import subprocess
+import win32serviceutil, win32api, win32con, win32gui
 
 TILE_WALLPAPER = "0" # 1 tiles image if style is center
+
+# Hide warning InsecureRequestWarning Unverified HTTPS request is being made
+warnings.simplefilter('ignore',InsecureRequestWarning)
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
                     datefmt='%Y/%d/%m %H:%M:%S', level=logging.ERROR)
 log = logging.getLogger()
 
-config = configparser.ConfigParser()
+config = configparser.ConfigParser(allow_no_value=True)
 try:
     config.read(os.getcwd() + "/apod_wallpaper.conf")
     api_key = config.get("api", "key")
     apod_url = config.get("api", "url")
-    px = config.get("proxy", "address") + ":" + config.get("proxy", "port")
-    proxy = {"https": "https://" + px, "http": "http://" + px}
-    path_local_proxy = config.get("proxy", "local_path")
+    proxy_addr = config.get("proxy", "address")
+    proxy_port = config.get("proxy", "port")
+    proxy = {"https": "https://" + proxy_addr + ":" + proxy_port, "http": "http://" + proxy_addr + ":" + proxy_port}
     download_path = config.get("default", "download_path")
     def_wallpaper_style = config.get("default", "wallpaper_style")
 except:
@@ -34,12 +38,15 @@ styles = {1: "0", 2: "2", 3: "6", 4: "10"}
 style_names = {1: "center", 2: "stretch", 3: "fit", 4: "fill"}
 
 
+def cntlm_running():
+    return win32serviceutil.QueryServiceStatus('cntlm', proxy_addr)[1] == 4
+
 def dispatch_http_get(url, p=None):
     global proxy
     r = None
     try:
         log.info("Dispatching HTTP GET Request %s... ", url)
-        r = requests.get(url, proxies=p)
+        r = requests.get(url, proxies=p,verify=False)
         log.info("Retrieved response")
     except Exception as e:
         if proxy is not None and p is None:
@@ -97,21 +104,18 @@ def set_windows_wallpaper(file_path, wallpaper_style):
     win32api.RegSetValueEx(key, "WallpaperStyle", 0, win32con.REG_SZ, styles[int(wallpaper_style)])
     win32api.RegSetValueEx(key, "TileWallpaper", 0, win32con.REG_SZ, TILE_WALLPAPER)
     win32gui.SystemParametersInfo(win32con.SPI_SETDESKWALLPAPER, file_path, 1 + 2)
-
+    log.info("Wallpaper set")
+    
 def exit_program():
     log.info("Exit")
     sys.exit()
 
 if __name__ == '__main__':
-    if path_local_proxy is not None:
-        if os.path.isfile(path_local_proxy):
-            log.info("Opening cntlm at " + path_local_proxy)
-            p=subprocess.Popen('\"'+path_local_proxy+'\"') 
-            log.info("done")
-        else:
-            log.error("Local proxy file not found " + path_local_proxy)
-            exit_program()
-            
+    if True and not cntlm_running():
+        log.info("Starting cntlm service")
+        win32serviceutil.StartService('cntlm', proxy_addr)
+        log.info("done")
+        
     log.info("Requesting content")
     content = dispatch_http_get(apod_url + "concept_tags=True&api_key=" + api_key)
     if content is None:
@@ -134,7 +138,9 @@ if __name__ == '__main__':
                 set_windows_wallpaper(filename, user_input)
                 # wait for user input (1 to 4 change the wallpaper style, otherwise exit)
                 user_input = input()
-    if path_local_proxy is not None:
-        log.info("closing cntlm")
-        p.terminate()
+    if True:
+        log.info("Stoping cntlm service")
+        win32serviceutil.StopService('cntlm', proxy_addr)
+        log.info("done")
+        
     exit_program()
